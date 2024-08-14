606be50096236f81e800db900b2db17bc618645d
Nick Thomas <nichoth@nichoth.com>
2024-08-14 10:33:21 -0700

    fix tag name in tests

--- test/index.ts
+++ test/index.ts
@@ -1,21 +1,21 @@
 import { test } from '@bicycle-codes/tapzero'
 import '../src/button-one.js'
 
-test('button element', t => {
+test('button-one element', t => {
     document.body.innerHTML += `
-        <button-element class="test">click this</button-element>
+        <button-one class="test">click this</button-one>
     `
 
-    const el = document.querySelector('button-element.test button')
-    t.ok(el, 'should find a button inside the `button-element`')
+    const el = document.querySelector('button-one.test button')
+    t.ok(el, 'should find a button inside the `button-one element`')
 })
 
 test('button element `a` tag', t => {
     document.body.innerHTML += `
-        <button-element class="a-tag" href="#example">click</button-element>
+        <button-one class="a-tag" href="#example">click</button-one>
     `
 
-    const el = document.querySelector('button-element.a-tag a')
+    const el = document.querySelector('button-one.a-tag a')
     t.equal(el?.getAttribute('href'), '#example',
         'should create an `a` tag given an `href` attribute')
 })
